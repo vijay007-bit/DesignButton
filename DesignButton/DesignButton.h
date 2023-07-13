e5e932d24cee6787956ae401619ca81541c5414b
Vijay Vikram Singh <svijayvikram00@gmail.com>
2023-07-13 22:37:08 +0530

    initial commit

---
+++ DesignButton/DesignButton.h
@@ -0,0 +1,18 @@
+//
+//  DesignButton.h
+//  DesignButton
+//
+//  Created by Vijay Singh on 13/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DesignButton.
+FOUNDATION_EXPORT double DesignButtonVersionNumber;
+
+//! Project version string for DesignButton.
+FOUNDATION_EXPORT const unsigned char DesignButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DesignButton/PublicHeader.h>
+
+
---
+++ DesignButtonExamples/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DesignButtonExamples
+//
+//  Created by Vijay Singh on 13/07/23.
+//
+
+import UIKit
+import DesignButton
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button_new: DesignButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
